c3a84fc1f9206edea8edcea02bef5099cbc94daf
fatmakhv <fatmaba@gmail.com>
2023-09-21 10:03:54 +0300

    Rearrangement

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,10 @@
-import { ReactNode } from 'react'
-import './globals.css'
+import { ReactNode } from "react";
+import "./globals.css";
 
-export default function RootLayout({
-  children,
-}: {
-  children: ReactNode
-}) {
-  return (
-    <html lang="en">
-      <body className="{body}">{children}</body>
-    </html>
-  )
+export default function RootLayout({ children }: { children: ReactNode }) {
+	return (
+		<html lang="en">
+			<body className="{body}">{children}</body>
+		</html>
+	);
 }
