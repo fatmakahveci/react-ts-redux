d58b4964e0a223cd8cb6a61a278b34e698bb3cae
fatmakhv <fatmaba@gmail.com>
2023-09-21 10:03:45 +0300

    Rearrangement

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,14 @@
 "use client";
 
-import './globals.css';
+import Counter from "@/app/components/Counter/Counter";
+import "./globals.css";
 
-const Home = ({ }): JSX.Element => {
-  return (
-    <div>
-    </div>
-  )
-}
+const Home = ({}): JSX.Element => {
+	return (
+		<>
+			<Counter />
+		</>
+	);
+};
 
 export default Home;
